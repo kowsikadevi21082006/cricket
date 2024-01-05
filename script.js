f5adc535943bc1d75c8c3026849a5134ee7ec14c
kowsikadevi21082006 <deva36rp@gamil.com>
2024-01-05 17:29:11 +0530

    done

---
+++ script.js
@@ -0,0 +1,43 @@
+function vs(){
+  var pop=document.getElementById("link")
+  pop.classList.toggle("show");
+}
+function st(){
+  var pop=document.getElementById("link2")
+  pop.classList.toggle("show");
+}
+function gg(){
+  var pop=document.getElementById("link3")
+  pop.classList.toggle("show");
+}
+function vk(){
+  var pop=document.getElementById("link4")
+  pop.classList.toggle("show");
+}
+function msd(){
+  var pop=document.getElementById("link5")
+  pop.classList.toggle("show");
+}
+function ys(){
+  var pop=document.getElementById("link6")
+  pop.classList.toggle("show");
+}
+function sr(){
+  var pop=document.getElementById("link7")
+  pop.classList.toggle("show");
+}
+function hs(){
+  var pop=document.getElementById("link8")
+  pop.classList.toggle("show");
+}
+function zk(){
+  var pop=document.getElementById("link9")
+  pop.classList.toggle("show");
+}function mp(){
+  var pop=document.getElementById("link10")
+  pop.classList.toggle("show");
+}
+function ss(){
+  var pop=document.getElementById("link11")
+  pop.classList.toggle("show");
+}
