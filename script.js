37f7e0d748fdd9a589b46ccb89824d8212297cdd
kowsikadevi21082006 <deva36rp@gamil.com>
2024-01-13 21:24:21 +0530

    done

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-function vs(){
-  var pop=document.getElementById("link")
-  pop.classList.toggle("show");
+function toggleVisibility(triggerElement, targetElement) {
+  triggerElement.addEventListener('click', function () {
+    if (targetElement.style.display == 'inherit') {
+      targetElement.style.display = 'none';
+    } else {
+      targetElement.style.display = 'inherit';
+    }
+  });
 }
-function st(){
-  var pop=document.getElementById("link2")
-  pop.classList.toggle("show");
-}
-function gg(){
-  var pop=document.getElementById("link3")
-  pop.classList.toggle("show");
-}
-function vk(){
-  var pop=document.getElementById("link4")
-  pop.classList.toggle("show");
-}
-function msd(){
-  var pop=document.getElementById("link5")
-  pop.classList.toggle("show");
-}
-function ys(){
-  var pop=document.getElementById("link6")
-  pop.classList.toggle("show");
-}
-function sr(){
-  var pop=document.getElementById("link7")
-  pop.classList.toggle("show");
-}
-function hs(){
-  var pop=document.getElementById("link8")
-  pop.classList.toggle("show");
-}
-function zk(){
-  var pop=document.getElementById("link9")
-  pop.classList.toggle("show");
-}function mp(){
-  var pop=document.getElementById("link10")
-  pop.classList.toggle("show");
-}
-function ss(){
-  var pop=document.getElementById("link11")
-  pop.classList.toggle("show");
-}
+
+const vs = document.getElementById('p');
+const vsimg = document.getElementById('ps');
+toggleVisibility(vs, vsimg);
+
+const vs2 = document.getElementById('p2');
+const vsimg2 = document.getElementById('ps2');
+toggleVisibility(vs2, vsimg2);
+
+const vs3 = document.getElementById('p3');
+const vsimg3 = document.getElementById('ps3');
+toggleVisibility(vs3, vsimg3);
+
+const vs4 = document.getElementById('p4');
+const vsimg4 = document.getElementById('ps4');
+toggleVisibility(vs4, vsimg4);
+
+
+const vs5 = document.getElementById('p5');
+const vsimg5 = document.getElementById('ps5');
+toggleVisibility(vs5, vsimg5);
+
+
+const vs6 = document.getElementById('p6');
+const vsimg6 = document.getElementById('ps6');
+toggleVisibility(vs6, vsimg6);
+
+
+const vs7 = document.getElementById('p7');
+const vsimg7 = document.getElementById('ps7');
+toggleVisibility(vs7, vsimg7);
+
+const vs8 = document.getElementById('p8');
+const vsimg8 = document.getElementById('ps8');
+toggleVisibility(vs8, vsimg8);
+
+const vs9 = document.getElementById('p9');
+const vsimg9 = document.getElementById('ps9');
+toggleVisibility(vs9, vsimg9);
+
+const vs10 = document.getElementById('p10');
+const vsimg10 = document.getElementById('ps10');
+toggleVisibility(vs10, vsimg10);
+
+const vs11 = document.getElementById('p11');
+const vsimg11 = document.getElementById('ps11');
+toggleVisibility(vs11, vsimg11);
